edac71140bddd7300117489620a42dd24f730b5a
Davidec0018 <d.caruso1@studenti.unisannio.it>
2022-07-06 01:39:16 +0200

    Spiegazione degli algoritmi di ordinamento finora utilizzati

--- src/main/java/it/algoritmi/AlgoritmiOrdinamento/InsertionSort.java
+++ src/main/java/it/algoritmi/AlgoritmiOrdinamento/InsertionSort.java
@@ -1,9 +1,36 @@
 package it.algoritmi.AlgoritmiOrdinamento;
 
 
-/* Complessità (worst case) = N^2
-   Complessità (average) = N^2
-   Complessità (best case) = N */
+    /* Spiegazione dell'algoritmo:
+     * 
+     * Si tratta di dividere concettualmente l'array in due parti,
+    di cui la sinistra ordinata e la destra da ordinare. Inizialmente la parte sinistra potrebbe essere nulla, ovvero
+    con un array tutto disordinato.
+    Questo algoritmo segue la logica di ordinamento che seguirebbe un essere umano.
+    Il ciclo esterno come al solito è per il confronto con il singolo elemento, il ciclo interno invece questa volta
+    va a spostare volta per volta nella giusta posizione l'elemento analizzato, shiftando eventualmente i più grandi
+    verso destra. Appunto un "inserzione" nel giusto punto dell'array, come suggerisce il nome.
+
+    Gli scambi ripetuti fino alla corretta posizione, effettuati con la funzione "exch", permettono di "shiftare"
+    gli elementi più grandi.
+
+    Il caso peggiore si ha quando l'array è reversed, cioè ordinato al contrario. In questo caso bisognerebbe portare ogni
+    elemento nella posizione diametralmente opposta, e quindi effettuare più scambi.
+    In questo caso infatti, bisogna eseguire confronti multipli per tutti gli elementi dell'array, quasi come se fosse un
+    selection sort, e infatti il numero di confronti coincide con (N^2 + N)/2, quindi con una complessità di O(n^2).
+
+    Nel caso medio, possiamo possiamo ipotizzare che gli elementi si trovino appunto a metà del loro massimo tragitto
+    e che quindi il numero di confronti medio possa essere la metà di quello nel caso peggiore, ovvero (N^2 + N)/4,
+    e quindi ancora con una complessità di O(n^2).
+
+    Nel caso migliore, l'array è già ordinato, quindi vengono eseguiti N-1 confronti, quindi complessità O(N) e logicamente
+    0 scambi.
+
+    Questo algoritmo lavora bene con array parzialmente ordinati, in particolare quando il numero di scambi effettuati
+    si avvicina, o è addirittura uguale al numero di inversioni effettuate, ovvero quando ogni scambio va a mettere
+    nella sua corretta posizione un elemento (invece di dover fare tanti scambi per una sola inversione).
+
+     */
    
 @SuppressWarnings("rawtypes")
 public class InsertionSort {
--- src/main/java/it/algoritmi/AlgoritmiOrdinamento/SelectionSort.java
+++ src/main/java/it/algoritmi/AlgoritmiOrdinamento/SelectionSort.java
@@ -1,7 +1,38 @@
 package it.algoritmi.AlgoritmiOrdinamento;
 
-/* Confronti in qualsiasi caso = N^2/2
-   Numero di scambi = N */
+   /* Spiegazione dell'algoritmo:
+    * 
+    * L'algoritmo di ordinamento più basilare in assoluto. Si tratta di dividere concettualmente l'array in due parti,
+    di cui la sinistra ordinata e la destra da ordinare. Inizialmente la parte sinistra potrebbe essere nulla, ovvero
+    con un array tutto disordinato.
+    Con i due cicli for implementiamo un controllo molto semplice. Per ogni nuovo elemento
+    da inserire nella parte ordinata andiamo alla ricerca del minimo assoluto nella parte disordinata, questo mediante il
+    ciclo for interno. Di conseguenza è necessario scorrere tutto l'array per fare i confronti. Una volta trovato il minimo
+    questo viene scambiato con l'elemento con cui era stato confrontato.
+    Si procede quindi facendo avanzare il ciclo più esterno, in maniera tale da non andare più a toccare la parte di
+    array già ordinata.
+
+    Ogni scambio (effettuato qui con la funzione "exch") pone un elemento nella sua posizione finale, quindi il numero
+    di scambi effettuati dall'algoritmo è pari a N-1 (dato che l'ultimo elemento non deve essere scambiato).
+    Questo numero di scambi è costante in ogni caso, dal momento che lo scambio avviene in ogni caso, stupidamente.
+    Infatti se il ciclo for interno non trova nessun valore più piccolo dell'elemento di confronto, il valore di min
+    all'uscita del ciclo interno è ancora pari a "i". Lo "scambio", se ancora così lo si può chiamare, avviene
+    sulla stessa casella dell'array. [SINCERAMENTE NON CAPISCO COSA COSTASSE AGGIUNGERE UN IF CHE CONTROLLASSE CHE MIN FOSSE
+    DIVERSO DA "I" PRIMA DI EFFETTUARE QUESTO INUTILE SCAMBIO. CAPISCO CHE QUESTO ALGORITMO NON SE LO CAGA NESSUNO 
+    MA DAI CAZZO]. 
+
+    Sul numero di confronti (complessità computazionale) invece possiamo dire che è pari a (N^2 + N)/2 (calcolato con una sommatoria di Gauss).
+    Da notare che i confronti (andando alla ricerca del minimo), devono avvenire in ogni caso, anche nel caso in cui 
+    l'array fosse già ordinato in partenza. La complessità di questo algoritmo infatti dipende dal numero di elementi
+    dell'array, e non da come sono disposti inizialmente.
+
+    La complessità temporale dell'algoritmo è dell'ordine O(N^2).
+    Ciò vuol dire che raddoppiando gli elementi nell'array, quadruplica il tempo necessari ad ordinarli, triplicando gli
+    elementi, il tempo sarà 9 volte maggiore.
+
+    Unica nota positiva di questo algoritmo il numero di scambi (N-1).
+
+    */
 
 @SuppressWarnings("rawtypes")
 public class SelectionSort {
